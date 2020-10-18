d2cadfd1968a83f64aed9d73bb65db362b79a47a
gvp-study <gvpsha@gmail.com>
2020-10-18 09:20:33 -0400

    Fixed BRISK. GVP

--- src/MidTermProject_Camera_Student.cpp
+++ src/MidTermProject_Camera_Student.cpp
@@ -67,14 +67,15 @@ int main(int argc, const char *argv[])
         dataBuffer.push_back(frame);
 	
         //// EOF STUDENT ASSIGNMENT
-        cout << "#1 : LOAD IMAGE INTO BUFFER done" << endl;
+        cout << "Loaded [" << imgIndex << "] " << imgFullFilename << endl;
 
         /* DETECT IMAGE KEYPOINTS */
 
         // extract 2D keypoints from current image
         vector<cv::KeyPoint> keypoints; // create empty feature list for current image
 //        string detectorType = "SHITOMASI";
-        string detectorType = "HARRIS";
+//        string detectorType = "HARRIS";
+	string detectorType = "BRISK";
 
         //// STUDENT ASSIGNMENT
         //// TASK MP.2 -> add the following keypoint detectors in file matching2D.cpp and enable string-based selection based on detectorType
@@ -88,7 +89,7 @@ int main(int argc, const char *argv[])
         {
 	    detKeypointsHarris(keypoints, imgGray, false);
         }
-	else if ((detectorType.compare("BRIEF") == 0) ||
+	else if ((detectorType.compare("BRISK") == 0) ||
 		 (detectorType.compare("ORB") == 0) ||
 		 (detectorType.compare("FREAK") == 0) ||
 		 (detectorType.compare("AKAZE") == 0) ||
