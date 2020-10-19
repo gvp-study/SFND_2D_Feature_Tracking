20d070ffe317c06a9c1816690170f5630e8c6d5d
gvp-study <gvpsha@gmail.com>
2020-10-19 15:08:12 -0400

    Fixed the bug in the FLANN check in matching2D_Student.cpp. GVP

--- src/MidTermProject_Camera_Student.cpp
+++ src/MidTermProject_Camera_Student.cpp
@@ -200,7 +200,7 @@ int main(int argc, const char *argv[])
 		    /* MATCH KEYPOINT DESCRIPTORS */
 
 		    vector<cv::DMatch> matches;
-		    string matcherType = "MAT_BF";        // MAT_BF, MAT_FLANN
+		    string matcherType = "MAT_FLANN";        // MAT_BF, MAT_FLANN
 		    string descriptorType2 = "DES_BINARY"; // DES_BINARY, DES_HOG
 		    string selectorType = "SEL_KNN";       // SEL_NN, SEL_KNN
 		    if(descriptorType.compare("SIFT") == 0)
--- src/matching2D_Student.cpp
+++ src/matching2D_Student.cpp
@@ -243,8 +243,8 @@ void matchDescriptors(std::vector<cv::KeyPoint> &kPtsSource, std::vector<cv::Key
     {
 	// OpenCV bug workaround : convert binary descriptors to floating point due
 	// to a bug in current OpenCV implementation
-	if (descSource.type() != CV_32F)
-	{ 
+	if (descSource.type() != CV_32F || descRef.type() != CV_32F)
+	{
 	    descSource.convertTo(descSource, CV_32F);
 	    descRef.convertTo(descRef, CV_32F);
 	}
